453dccdef810d49b86eeb1943af56786121b0f37
Binybana <binybana44@gmail.com>
2023-10-24 08:41:26 +0300

    entered validation javascript but is not working

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
-#name{
-    
+var index;
+var user{
+    names[20];
+    email[20];
+} 
+function validate(){ 
+    alert("i'm in");
+    var name = document.getElementById("name").value;
+    var len=name.length;
+    if(len>10 || len<5){
+        alert("Enter a valid name")
+        return false;
+    }
+    elses[
+        user.names[index]=name;
+        index++;
+    ]
+    alert(name[index])
+}
+function hi(){
+    alert("how are you?")
 }
